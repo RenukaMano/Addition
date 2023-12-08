d37fb2b028c847f231110cdc2549878c8236191e
anushka <anushka@localhost>
2023-12-08 21:36:54 +1100

    Initial Commit

---
+++ Addition/src/com/renuka/addition.java
@@ -0,0 +1,14 @@
+package com.renuka;
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=4;
+		int c;
+		System.out.println(c=a+b);
+
+	}
+
+}
